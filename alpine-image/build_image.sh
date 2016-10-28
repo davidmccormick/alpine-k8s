fafe6d72f1a3aa85452f6d09ad2fd90219c419bc
davidmccormick <davidemccormick@gmail.com>
2016-10-28 14:37:42 +0100

    Fix uploads to atlas, write Vagrant file with right box name as part of the build

--- alpine-image/build_image.sh
+++ alpine-image/build_image.sh
@@ -2,6 +2,20 @@
 
 set -e
 
+FORCE="false"
+
+# read command line args
+for i in "$@"
+do
+case $i in
+    --force) FORCE="true"
+    ;;
+    *) echo "option $i unknown"
+       exit 1
+    ;;
+esac
+done
+
 echo "Builder for alpine-kubernetes vagrant box"
 
 REQUIRED="curl head awk sed tail packer"
@@ -33,7 +47,7 @@ ALPINE_LATEST_SHA256=$(curl -k -s  http://dl-cdn.alpinelinux.org/alpine/.latest.
 ALPINE_VERSION="${ALPINE_LATEST_ISO%-x86_64.iso*}"
 ALPINE_VERSION="${ALPINE_VERSION##*-}"
 
-DOCKER_VERSION=$(curl -s http://dl-4.alpinelinux.org/alpine/edge/community/x86_64/ | grep "docker-[0-9]" | sed -e 's/.apk.*$//' | sed -e 's/^.*docker-//')
+DOCKER_VERSION=$(curl -s http://dl-4.alpinelinux.org/alpine/edge/community/x86_64/ | grep "docker-[0-9]" | sed -e 's/.apk.*$//' | sed -e 's/^.*docker-//' | sed -e 's/-.*$//')
 
 KUBERNETES_VERSION=$(curl -s -k -L https://storage.googleapis.com/kubernetes-release/release/stable.txt)
 KUBELET_URL="https://storage.googleapis.com/kubernetes-release/release/${KUBERNETES_VERSION}/bin/linux/amd64/kubelet"
@@ -41,6 +55,8 @@ KUBECTL_URL="https://storage.googleapis.com/kubernetes-release/release/${KUBERNE
 
 KUBEADM_LATEST=$(curl -L -s https://storage.googleapis.com/kubeadm | python -c 'import sys;import xml.dom.minidom;s=sys.stdin.read();print xml.dom.minidom.parseString(s).toprettyxml()' | grep "<Key>.*amd64/kubeadm" | tail -1 | sed -e 's/^.*<Key>//' | sed -e 's/<\/Key>.*$//')
 KUBEADM_URL="https://storage.googleapis.com/kubeadm/${KUBEADM_LATEST}"
+
+ATLAS_BOX="alpine-${ALPINE_VERSION}-docker-${DOCKER_VERSION}-kubernetes-${KUBERNETES_VERSION}"
 
 if [[ -d "output-virtualbox-iso" ]]; then
   echo "Removing existing output-virtualbox-iso"
@@ -60,6 +76,107 @@ echo "KUBELET_URL = ${KUBELET_URL}"
 echo "KUBECTL_URL = ${KUBECTL_URL}"
 echo "KUBEADM_URL = ${KUBEADM_URL}"
 echo ""
+echo "ATLAS:-"
+echo "ATLAS_USER : ${ATLAS_USER}"
+echo "ATLAS_TOKEN: ${ATLAS_TOKEN}"
+echo "ATLAS_BOX  : ${ATLAS_BOX}"
+echo ""
+
 export ALPINE_VERSION DOCKER_VERSION KUBERNETES_VERSION ALPINE_LATEST_ISO ALPINE_LATEST_SHA256 KUBELET_URL KUBECTL_URL KUBEADM_URL
 
-packer build alpine-kubernetes.json && packer push alpine-kubernetes.json
+set +e
+# check login to atlas...
+if ! curl --fail -k -s https://atlas.hashicorp.com/api/v1 \
+  -X GET  \
+  -H "X-Atlas-Token: ${ATLAS_TOKEN}" >/dev/null
+then
+  echo "Sorry, I could not connect to Hashicorp Atlas - are you username and token correct?"
+  exit 1
+fi
+
+# check if box exists
+if ! curl --fail -k -s https://atlas.hashicorp.com/api/v1/box/dmcc/${ATLAS_BOX} -X GET -H "X-Atlas-Token: ${ATLAS_TOKEN}" >/dev/null
+then
+  echo "Creating atlas box ${ATLAS_BOX}"
+  # assume it doesn't exist and create it as a new public box
+  if ! curl --fail -k -v https://atlas.hashicorp.com/api/v1/boxes -X POST -H "X-Atlas-Token: ${ATLAS_TOKEN}" -d box[name]="${ATLAS_BOX}" -d box[is_private]='false'
+  then
+    echo "Sorry couldn't create the new box ${ATLAS_BOX}"
+    exit 1
+  fi
+else
+  if [[ "$FORCE" == "true" ]]; then
+    echo "Deleting box ${ATLAS_BOX}"
+    if ! curl --fail -k -s https://atlas.hashicorp.com/api/v1/box/dmcc/${ATLAS_BOX} -X DELETE -H "X-Atlas-Token: ${ATLAS_TOKEN}" >/dev/null
+    then
+     echo "Failed to delete box ${ATLAS_BOX}"
+     exit 1
+    fi
+    echo "Re-creating box ${ATLAS_BOX}"
+    if ! curl --fail -k -s https://atlas.hashicorp.com/api/v1/boxes -X POST -H "X-Atlas-Token: ${ATLAS_TOKEN}" -d box[name]="${ATLAS_BOX}"  -d box[is_private]='false' >/dev/null
+    then
+      echo "Sorry couldn't create box ${ATLAS_BOX}"
+      exit 1
+    fi
+  else
+    echo "Box ${ATLAS_BOX} already exists."
+    exit 1
+  fi
+fi
+
+set -e 
+
+echo "Writing the Vagrant file"...
+cat >Vagrantfile <<EOT
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+Vagrant.configure(2) do |config|
+
+  config.vm.define "alpine" do |alpine|
+
+    alpine.vm.box = "dmcc/${ATLAS_BOX}"
+
+    #alpine.ssh.username = "vagrant"
+    #alpine.ssh.password = "vagrant"
+
+    # NOTE:
+    #   there are *no* guest additions installed
+    #   for alpine linux - the guest additions
+    #   installer fails. once workarounds are
+    #   identified the vbga will be added
+    #   to the base box.
+    #
+    # since there are no vbga. if the vagrant-alpine plugin
+    # is installed it can at least configure the system to
+    # enable shared folders.
+    #
+    alpine.vm.synced_folder ".", "/vagrant", disabled: true
+    #
+    # after \`vagrant plugin install vagrant-alpine\`
+    # comment the disabled synced_folder line above and
+    # uncomment the following two lines
+    #
+    # alpine.vm.network "private_network", ip: "172.28.128.250"
+    # alpine.vm.synced_folder ".", "/vagrant", type: "nfs"
+
+    alpine.vm.provider "virtualbox" do |vb|
+      vb.name = 'Alpine'
+      vb.cpus = 1
+      vb.memory = 1024
+      vb.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
+      # Display the VirtualBox GUI when booting the machine
+      # vb.gui = true
+    end
+  end
+
+  if Vagrant.has_plugin?("vagrant-vbguest")
+    config.vbguest.auto_update = false
+  end
+
+end
+EOT
+
+# build the new box
+packer build alpine-kubernetes.json 
+echo "build_image completed ok"
