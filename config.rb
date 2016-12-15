29b2bb884a9a088ded6ae4cf11c6aff249bb03a2
davidmccormick <davidemccormick@gmail.com>
2016-12-15 20:58:40 +0000

    Too little RAM allocated

--- config.rb
+++ config.rb
@@ -1,10 +1,10 @@
 $kubernetes_version="v1.5.1"
 
 $master_count=1
-$master_memory=512
+$master_memory=2048
 $master_cpu=2
 
 $minion_count=1
-$minion_memory=1024
+$minion_memory=2048
 $minion_cpu=2
 
