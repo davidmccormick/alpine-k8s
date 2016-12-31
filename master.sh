f45ce100fd097c6631396cc685f14736dd82c8db
David McCormick <david.mccormick@zopa.com>
2016-12-31 18:31:07 +0000

    Canal and others as addons

--- master.sh
+++ master.sh
@@ -16,28 +16,43 @@ add_cluster_service_label() {
 
 install_addon() {
 	local URL=$1
-	local MANIFEST=$2
+	local MAN=$2
 
-	curl -k -L -s ${URL} >/etc/kubernetes/addons/${MANIFEST}
+	echo "Downloading addon $URL..."
+	curl -k -L -s ${URL} >/etc/kubernetes/addons/${MAN}
 	#separate multiple objects in one 1 file into multiple manifests
-	if grep -q "^---" /etc/kubernetes/addons/${MANIFEST}; then
-		local NUM_OBJECTS=$(cat /etc/kubernetes/addons/${MANIFEST}  | grep "^---" | wc -l)
+	if grep -q "^---" /etc/kubernetes/addons/${MAN}; then
+		local NUM_OBJECTS=$(cat /etc/kubernetes/addons/${MAN}  | grep "^---" | wc -l)
 
 		# loop over the objects creating separate files file1.yaml file2.yaml
 		local l=0
 		while [ $l -le $NUM_OBJECTS ]
 		do  
-			local NEW_NAME=${MANIFEST/%.yaml/$l.yaml}
-			echo "Creating new manifest ${NEW_NAME}" 
-			cat /etc/kubernetes/addons/${MANIFEST} | awk 'BEGIN{count=0} ($0 ~ /^---/){count++;next} ( count == '$l' ){print}' >/etc/kubernetes/addons/${NEW_NAME}
+			local NEW_NAME=${MAN/%.yaml/$l.yaml}
+			echo "Creating new addon manifest ${NEW_NAME}" 
+			cat /etc/kubernetes/addons/${MAN} | awk 'BEGIN{count=0} ($0 ~ /^---/){count++;next} ( count == '$l' ){print}' >/etc/kubernetes/addons/${NEW_NAME}
 			add_cluster_service_label "${NEW_NAME}"
 			l=$((l+=1))
 		done
-		rm -f /etc/kubernetes/addons/${MANIFEST}
+		rm -f /etc/kubernetes/addons/${MAN}
 	else
-		add_cluster_service_label /etc/kubernetes/addons/${MANIFEST}
+		add_cluster_service_label ${MAN}
 	fi
 }
+
+# install the cluster with kubeadm
+echo "Running kubeadm init to configure kubernetes..."
+echo "MY_IP is ${MY_IP}"
+echo "cluster_token is ${KUBE_TOKEN}"
+echo "Running: kubeadm init --api-advertise-addresses=${MY_IP} --api-external-dns-names=master.example.com --token=${KUBE_TOKEN}"
+kubeadm init --api-advertise-addresses=${MY_IP} --api-external-dns-names=master.example.com --token=${KUBE_TOKEN} --use-kubernetes-version ${KUBERNETES_VERSION} | tee /root/kubeadm_init.log
+
+#copy kubeconfig for root's usage
+mkdir -p /root/.kube
+cp /etc/kubernetes/admin.conf /root/.kube/config
+
+echo "Patching the apiserver manifest to advertise the master on the right address..."
+sed -e 's/"--allow-privileged",/"--allow-privileged","--advertise-address='${MY_IP}'",/' -i /etc/kubernetes/manifests/kube-apiserver.json
 
 echo "Preparing Addons..."
 mkdir -p /etc/kubernetes/addons
@@ -59,20 +74,6 @@ done
 # Install the addon manager as a direct kubelet manifest
 echo "Installing Addon Manager - to install/manage addons"
 curl -k -L -s https://raw.githubusercontent.com/kubernetes/kubernetes/master/cluster/saltbase/salt/kube-addons/kube-addon-manager.yaml >/etc/kubernetes/manifests/addon-manager.yaml
-
-# Actually install the cluster with kubeadm
-echo "Running kubeadm init to configure kubernetes..."
-echo "MY_IP is ${MY_IP}"
-echo "cluster_token is ${KUBE_TOKEN}"
-echo "Running: kubeadm init --api-advertise-addresses=${MY_IP} --api-external-dns-names=master.example.com --token=${KUBE_TOKEN}"
-kubeadm init --api-advertise-addresses=${MY_IP} --api-external-dns-names=master.example.com --token=${KUBE_TOKEN} --use-kubernetes-version ${KUBERNETES_VERSION} | tee /root/kubeadm_init.log
-
-#copy kubeconfig for root's usage
-mkdir -p /root/.kube
-cp /etc/kubernetes/admin.conf /root/.kube/config
-
-echo "Patching the apiserver manifest to advertise the master on the right address..."
-sed -e 's/"--allow-privileged",/"--allow-privileged","--advertise-address='${MY_IP}'",/' -i /etc/kubernetes/manifests/kube-apiserver.json
 
 # Remove kubelet restarter
 [[ -f "/etc/periodic/1min/kubelet" ]] && rm -f /etc/periodic/1min/kubelet
